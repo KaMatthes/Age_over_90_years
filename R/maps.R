731e4d0bc5f7202539d87aa09289a3223cb2fd56
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2022-03-10 19:22:07 +0100

    maps

---
+++ R/maps.R
@@ -0,0 +1,139 @@
+function_mapsPLZ <- function(Year, Place) {
+
+  # load data
+  load("../data/data_age90.RData")
+  #load("../data/data_age90.RData")
+  
+  brk_alt <- c(0,300,600,900,1200,1500,1800,2200)
+  
+  
+  data_age90_map <- data_age90 %>%
+    dplyr::mutate(B_Hoehe_cat = cut(B_Hoehe,breaks=brk_alt),
+           W_Hoehe_cat = cut(W_Hoehe,breaks=brk_alt),
+           G_Hoehe_cat = cut(G_Hoehe,breaks=brk_alt)) %>%
+    filter(Erhebungsjahr == Year)
+  # load map switzerland
+  
+   map_swiss <- read_sf("../data/maps/Timo/g2l15.shp")
+   #map_swiss <- read_sf("../data/maps/Timo/g2l15.shp")
+   
+   map_canton <- read_sf("../data/maps/Timo/g2k15.shp") %>%
+     mutate(Language = KTNR,
+            Language = as.character(Language),
+            Language = recode(Language,
+                              "19" = "German",
+                              "15" = "German",
+                              "16" = "German",
+                              "13" = "German",
+                              "12" = "German",
+                              "2" = "German",
+                              "10" = "French",
+                              "25" = "French",
+                              "8" = "German",
+                              "18" = "German",
+                              "26" = "French",
+                              "3" = "German",
+                              "24" = "French",
+                              "7" = "German",
+                              "6" = "German",
+                              "14" = "German",
+                              "5" = "German",
+                              "11" = "German",
+                              "17" = "German",
+                              "20" = "German",
+                              "21" = "Italian",
+                              "4" = "German",
+                              "23" = "French",
+                              "22" = "French",
+                              "9" = "German",
+                              "1" = "German"),
+            Language = as.factor(Language))
+
+   # map_lake <- read_sf("data/maps/Timo/g2s15.shp")
+   map_lake <- read_sf("../data/maps/Timo/g2s15.shp")
+   
+
+  relief <- raster("../data/maps/Timo/02-relief-ascii.asc") %>%
+    # hide relief outside of Switzerland by masking with country borders
+    mask(map_swiss) %>%
+    as("SpatialPixelsDataFrame") %>%
+    as.data.frame() %>%
+    rename(value = `X02.relief.ascii`)
+
+  
+  # decide which place
+
+  if(Place=="Birthplace") {
+    datMaps <-  data_age90_map %>%
+      filter(!G_E==0)
+
+    plot_map <- ggplot()+
+      geom_sf(data=  map_canton, aes(fill= Language),alpha=1,col="black", size=0.1)+
+      geom_raster(data = relief,inherit.aes = FALSE,aes(x = x,y = y,alpha = value), fill="grey10")+
+      scale_alpha(name = "",
+                  range = c(0.6, 0),
+                  guide = "none") +
+      geom_sf(data=  map_lake, fill = "#D6F1FF",color = "transparent") +
+      geom_point(data=datMaps,aes(x=G_E, y= G_N, col=G_Hoehe_cat), lwd=4)+
+      ggtitle(paste(Year,"Birthplace"))+
+      scale_colour_manual("Altitude:",
+                          values = col7magma,
+                          breaks=c("(0,300]","(300,600]","(600,900]","(900,1.2e+03]","(1.2e+03,1.5e+03]","(1.5e+03,1.8e+03]","(1.8e+03,2.2e+03]"),
+                          labels=c("0-299","300-599","600-899","900-1199","1200-1499","1500-1799",">1799"))+
+      scale_fill_manual("Language:",
+                        values=col3grey)+
+      theme_bw()+
+      theme(
+        panel.background=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        axis.title=element_blank(),
+        axis.line=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        panel.border = element_blank(),
+        legend.text=element_text(size=size_legend),
+        legend.title =element_text(size=size_legend),
+        plot.title = element_text(size =size_ggtitle),
+        legend.position = "bottom")
+
+  }
+
+  else if(Place=="Residential") {
+    datMaps <-   data_age90_map %>%
+      filter(!W_E==0)
+    
+    plot_map<- ggplot()+
+      geom_sf(data=  map_canton, aes(fill= Language),alpha=1,col="black", size=0.1)+
+      geom_raster(data = relief,inherit.aes = FALSE,aes(x = x,y = y,alpha = value), fill="grey10")+
+      scale_alpha(name = "",
+                  range = c(0.6, 0),
+                  guide = "none") +
+      geom_sf(data=  map_lake, fill = "#D6F1FF",color = "transparent") +
+      geom_point(data=datMaps,aes(x=W_E, y= W_N, col=W_Hoehe_cat), lwd=4)+
+      ggtitle(paste(Year,"Residential"))+
+      scale_colour_manual("Altitude:",
+                        values = col7magma,
+                        breaks=c("(0,300]","(300,600]","(600,900]","(900,1.2e+03]","(1.2e+03,1.5e+03]","(1.5e+03,1.8e+03]","(1.8e+03,2.2e+03]"),
+                        labels=c("0-299","300-599","600-899","900-1199","1200-1499","1500-1799",">1799"))+
+      scale_fill_manual("Language:",
+                        values=col3grey)+
+      theme_bw()+
+      theme(
+        panel.background=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.grid.major=element_blank(),
+        axis.title=element_blank(),
+        axis.line=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        panel.border = element_blank(),
+        legend.text=element_text(size=size_legend),
+        legend.title =element_text(size=size_legend),
+        plot.title = element_text(size =size_ggtitle),
+        legend.position = "bottom")
+}
+  return(plot_map)
+ 
+  
+}
---
+++ master.R
@@ -0,0 +1,83 @@
+.libPaths(c("H:/Documents/R/win-library/4.1", "C:/Program Files/R/R-4.1.2/library"))
+
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(openxlsx)
+library(raster)
+library(sf)
+library(sp)
+library(viridis)
+library(scales)
+library(cowplot)
+library(rmarkdown)
+library(kableExtra)
+library(lubridate)
+
+# 
+# col7 <- c("#ffffb2","#c7e9b4","#7fcdbb","#41b6c4","#225ea8", "#253494","#081d58")
+# col7dark <- c("#b35806",
+#   "#e08214",
+#   "#fdb863",
+#   # "#fee0b6",
+#   "#d8daeb",
+#   "#b2abd2",
+#   "#8073ac",
+#   "#542788")
+# 
+# col7green <- c("#c7e9c0",
+#   "#a1d99b",
+#   "#74c476",
+#   "#41ab5d",
+#   "#238b45",
+#   "#006d2c",
+#   "#00441b")
+# 
+# col7blue <- c(
+# "#c7e9b4",
+# "#7fcdbb",
+# "#41b6c4",
+# "#1d91c0",
+# "#225ea8",
+# "#253494",
+# "#081d58")
+# 
+# col7pink <- c("#feebe2",
+#   "#fcc5c0",
+#   "#fa9fb5",
+#   "#f768a1",
+#   "#dd3497",
+#   "#ae017e",
+#   "#7a0177")
+# 
+# col7red <- c("#fee5d9",
+#   "#fcbba1",
+#   "#fc9272",
+#   "#fb6a4a",
+#   "#ef3b2c",
+#   "#cb181d",
+#   "#99000d")
+# 
+# col7grey <- c("#d9d9d9",
+#   "#bdbdbd",
+#   "#969696",
+#   "#737373",
+#   "#525252",
+#   "#252525",
+#   "#000000")
+
+col3grey <- c("#bdbdbd","#f0f0f0", "#737373")
+col7magma <- viridis(7, alpha = 1, begin = 1, end = 0, direction = 1, option = "magma")
+# col7viridis <- viridis(7, alpha = 1, begin = 1, end = 0, direction = 1, option = "viridis")
+# col7rocket <- viridis(7, alpha = 1, begin = 1, end = 0, direction = 1, option = "rocket")
+# col7cividis <- viridis(7, alpha = 1, begin = 1, end = 0, direction = 1, option = "cividis")
+
+brk_alt <- c(0,300,600,900,1200,1500,1800,2200)
+
+size_legend <- 20
+size_ggtitle <- 35
+
+
+source("R/maps.R")
+
+render(paste0("R/Age_over_90_years.Rmd"), output_file = paste0("../output/",today(),"_Age_over_90_years.html"))
