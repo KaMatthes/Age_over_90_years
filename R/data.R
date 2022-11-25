a99903d4dd791d91a6f495514a39f660d069eadd
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2022-11-25 14:51:46 +0100

    cleaned up

--- R/data.R
+++
@@ -1,5 +0,0 @@
-data_age90 <- readxl::read_excel("data_raw/Data_1888_1900.xlsx", sheet="Liste 1888 & 1900") %>%
-  dplyr::select(-ID, -Ordnungszahl, - Ordnungszahl2)
-
-save(data_age90 ,file=paste0("data/data_age90.RData"))
-write.xlsx(data_age90,file=paste0("data/data_age90.xlsx"),rowNames=FALSE, overwrite = TRUE)
